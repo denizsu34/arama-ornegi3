76a0599620b58f45d511c3d230c5cd26148c05f7
denizsu34 <148059058+denizsu34@users.noreply.github.com>
2023-11-20 12:09:14 +0300

    Add files via upload

---
+++ combox örnek 3_1681/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combox_örnek_3_1681
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            bool durum = false;
+            int sayaç = 0;
+
+            for (int i = 0; i < lbAdlar.Items.Count; i++)
+            {
+                string aranan = lbAdlar.Items[i].ToString();
+                if (aranan == txtAranan.Text)
+                {
+                    durum = true;
+                    sayaç++;//aranan kişiyi bir arttır
+                }
+                // döngü bittikten sonra
+                if (durum)
+                {
+                    lblSonuc.Text = "aradığınız kişiden " + sayaç + "tane var.";
+                }
+                else
+                {
+                    lblSonuc.Text = "aradığınız kişi yok.";
+                }
+            }
+        }
+    }
+ }
+   
+
+
+
